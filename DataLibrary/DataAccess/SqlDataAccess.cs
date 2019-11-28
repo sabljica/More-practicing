392ba01c40e1c5f0d5bb990dbe48ad3c58bc2099
sabljica <tarik.sabljica7@gmail.com>
2019-11-28 12:23:24 +0100

    First commit

---
+++ DataLibrary/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dapper;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace DataLibrary.DataAccess
+{
+    public static class SqlDataAccess
+    {
+        public static string GetConnectionString(string connectionName="MVCDemoDB")
+        {
+            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+
+        public static List<T> LoadData<T>(string sql)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Query<T>(sql).ToList();
+            }
+        }
+
+        public static int SaveData<T>(string sql, T data)
+        {
+            using (IDbConnection cnn = new SqlConnection(GetConnectionString()))
+            {
+                return cnn.Execute(sql, data);
+            }
+        }
+
+
+    }
+
+
+}
---
+++ MVCApp/Models/EmployeeModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models
+{
+    public class EmployeeModel
+    {
+        [Display(Name ="Employee ID")]
+        [Range(1000000, 9999999,ErrorMessage ="Invalid entrance")]
+        public int EmployeeId { get; set; }
+
+
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage ="Feild is required")]
+        public string FirstName { get; set; }
+
+
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Feild is required")]
+        public string LastName { get; set; }
+
+
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Feild is required")]
+        public string EmailAddress { get; set; }
+ 
+
+
+        [Display(Name = "Confirm Email")]
+        [Compare("EmailAddress",ErrorMessage ="The email and confirm email got to match")]
+        public string ConfirmEmail { get; set; }
+
+
+
+        [Required(ErrorMessage ="You must enter a password")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength =10, ErrorMessage ="Password must be between 10 and 100 characters")]
+        public string Password { get; set; }
+
+
+
+
+        [Display(Name = "Confirm password")]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="Password doesn't match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
